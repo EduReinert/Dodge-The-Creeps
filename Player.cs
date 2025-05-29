1c5647141b43afbdd47d577e7efe660215e3acd5
Eduardo Reinert <eduardo.a.reinertt@gmail.com>
2025-05-29 18:33:15 -0300

    Main game scene

--- Player.cs
+++ Player.cs
@@ -18,7 +18,7 @@ public partial class Player : Area2D
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
-		// Hide();
+		Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
